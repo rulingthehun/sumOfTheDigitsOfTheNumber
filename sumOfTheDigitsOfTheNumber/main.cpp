59c9cb7c2807872ddb335d368855afc04ea109ce
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-04-05 19:54:55 +0300

    Initial Commit

---
+++ sumOfTheDigitsOfTheNumber/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  sumOfTheDigitsOfTheNumber
+//
+//  Created by Burak Gaznepoğlu on 5.04.2023.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    string x;
+    int z = 0, num = 1;
+    cout << "Enter the value of number : ";
+    cin >> x;
+    
+    for(int i = 0; i < x.size() ; i++){
+        int intX = stoi(x);
+        z +=  (intX / num) % 10;
+        num *= 10;
+    }
+    cout << z <<endl;
+    return 0;
+}
