4f37dac406f3cee28962617c9fbb80a8820c90c2
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-04-05 20:18:27 +0300

    file is formatted

--- sumOfTheDigitsOfTheNumber/main.cpp
+++ sumOfTheDigitsOfTheNumber/main.cpp
@@ -10,14 +10,16 @@ using namespace std;
 int main() {
     string x;
     int z = 0, num = 1;
+
     cout << "Enter the value of number : ";
     cin >> x;
-    
-    for(int i = 0; i < x.size() ; i++){
+
+    for (int i = 0; i < x.size(); i++) {
         int intX = stoi(x);
         z +=  (intX / num) % 10;
         num *= 10;
     }
-    cout << "Sum of the entered number digits is = " << z <<endl;
+
+    cout << "Sum of the entered number digits is = " << z << endl;
     return 0;
 }
